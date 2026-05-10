4df78347df879ea740e165f377d7af281afc66c2
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2026-05-10 13:45:45 -0300

    feat: update deployment configuration and adjust base URLs for components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isGithubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: isGithubPages ? 'https://giovannitrevisan.github.io' : undefined,
+  base: isGithubPages ? '/vw-suplementos-bh' : undefined,
+});
