350df5caa0715f8826747782630d9b908411daf5
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2026-05-10 14:07:34 -0300

    fix: update base URLs for logo and background images in Header and Hero components

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ const isGithubPages = process.env.DEPLOY_TARGET === 'github-pages';
 // https://astro.build/config
 export default defineConfig({
   site: isGithubPages ? 'https://giovannitrevisan.github.io' : undefined,
-  base: isGithubPages ? '/vw-suplementos-bh' : undefined,
+  base: isGithubPages ? '/vw-suplementos-bh/' : undefined,
 });
