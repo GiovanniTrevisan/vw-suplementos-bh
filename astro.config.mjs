87a34d8358533ca8f547a9112cabc55245e086ce
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2026-05-11 18:42:07 -0300

    fix: remove DEPLOY_TARGET dependency and simplify site configuration in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const isGithubPages = process.env.DEPLOY_TARGET === 'github-pages';
-
 // https://astro.build/config
 export default defineConfig({
-  site: isGithubPages ? 'https://giovannitrevisan.github.io' : 'https://vwsuplementosbh.com.br/',
-  base: isGithubPages ? '/vw-suplementos-bh/' : undefined,
+  site: 'https://vwsuplementosbh.com.br/',
 });
