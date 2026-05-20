3fed10f24d66e38637d62ac6d51668621ef76ff5
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2026-05-20 07:44:20 -0300

    fix: update site URL and trailingSlash configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vwsuplementosbh.com.br/',
-  trailingSlash: 'ignore',
+  site: 'https://vwsuplementosbh.com.br',
+  trailingSlash: 'never',
 });
